012defa906ff7b2cf35e1bb9d8ffde1dd6dbffc3
pedroblima <pedrob@iesb.edu.br>
2023-11-03 15:39:58 -0300

    Lista de tarefas

--- src/router/Routers.js
+++ src/router/Routers.js
@@ -7,6 +7,8 @@ import React from 'react'
 import StackPessoas from '../screens/ListaPessoas/StackPessoa'
 import StackAsync from '../screens/PessoaAsync/StackAsync'
 
+import ListaCarAsync from '../screens/ListaCarro/ListaCarAsync'
+
 
 const Drawer = createDrawerNavigator()
 
@@ -15,7 +17,8 @@ export default function DrawerRoutes() {
         <Drawer.Navigator initialRouteName='StackPessoa'>
             <Drawer.Screen name="Lista de Pessoa" component={StackPessoas} />
             <Drawer.Screen name="Lista com Async" component={StackAsync} />
+            <Drawer.Screen name="Lista tarefas" component={ListaCarAsync} />
         </Drawer.Navigator>
 
     )
-}
+} 
