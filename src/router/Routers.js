4d82302fbca2bff1565f3997e0dfac6f37c8b604
Lucat22 <lucalvins1@gmail.com>
2023-11-05 02:16:32 -0300

    Exercício CRUD

--- src/router/Routers.js
+++ src/router/Routers.js
@@ -1,13 +1,9 @@
-
 import { createDrawerNavigator } from '@react-navigation/drawer'
 import React from 'react'
 
 
 
 import StackPessoas from '../screens/ListaPessoas/StackPessoa'
-import StackAsync from '../screens/PessoaAsync/StackAsync'
-
-import ListaCarAsync from '../screens/ListaCarro/ListaCarAsync'
 
 
 const Drawer = createDrawerNavigator()
@@ -15,10 +11,8 @@ const Drawer = createDrawerNavigator()
 export default function DrawerRoutes() {
     return (
         <Drawer.Navigator initialRouteName='StackPessoa'>
-            <Drawer.Screen name="Lista de Pessoa" component={StackPessoas} />
-            <Drawer.Screen name="Lista com Async" component={StackAsync} />
-            <Drawer.Screen name="Lista tarefas" component={ListaCarAsync} />
+            <Drawer.Screen name="StackPessoa" component={StackPessoas} />
         </Drawer.Navigator>
 
     )
-} 
+}
